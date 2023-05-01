05b7c04e34e96b69a681cb2bda1f9c6cfd8943af
Matúš <mymail@gmail.com>
2023-05-01 18:08:43 +0200

    Initial Project

---
+++ src/main/java/sk/meski/epiclands/EpiclandsApplication.java
@@ -0,0 +1,13 @@
+package sk.meski.epiclands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiclandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiclandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/meski/epiclands/EpiclandsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.meski.epiclands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiclandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
